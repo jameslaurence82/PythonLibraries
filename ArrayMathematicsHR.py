486f49c785a3101975b8976c318e6ac779439e92
James Laurence <jameslaurence82@gmail.com>
2023-10-25 09:56:12 -0300

    learning more numpy codings

---
+++ ArrayMathematicsHR.py
@@ -0,0 +1,10 @@
+# https://www.hackerrank.com/challenges/np-array-mathematics/problem?isFullScreen=true
+# You are given two integer arrays,  and  of dimensions X.
+# Your task is to perform the following operations:
+
+# Add (a + b)
+# Subtract (a - b)
+# Multiply (a * b)
+# Integer Division (a / b)
+# Mod (a % b)
+# Power (a ** b)
--- numpytest2.py
+++ numpytest2.py
@@ -31,3 +31,45 @@ print(stdev)
 print("using std() from numpy library to find standard deviation of array")
 std = np.std(a, axis = 1)
 print(std)
+
+a = np.arange(1, 5)
+result = np.prod(a)
+
+print(a)
+print(f'result={result}')
+
+result = np.prod([
+    [1, 2],
+    [3, 4]
+],axis = 1)
+print("axis 1")
+print(f'result={result}')
+
+result = np.prod([
+    [1, 2],
+    [3, 4]
+],axis = 0)
+print("axis 0")
+print(f'result={result}')
+
+# Selecting numbers to include in the product
+# nan avoids not a number elements
+a = np.array([np.nan, 3, 4])
+result = np.prod(a, where=[False, True, True])
+print(result)
+
+result = np.prod(np.arange(1,34))
+print(result)
+
+"""
+Minimum and Maximum
+"""
+a = np.array([
+    [1, 2, 3],
+    [4, 5, 6]
+])
+print(f"arry a is:\n {a}")
+min = np.amin(a)
+print(min)
+max = np.amax(a)
+print(max)
